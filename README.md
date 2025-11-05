a4a932ea578b576d8856476d9ec4b0da68f254a6
Theyka <abutalibsiriyev2003@gmail.com>
2025-11-05 18:36:51 +0400

    backend done

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# SecurityOnlineTracking-
+# SecurityOnlineTracking
+
+Before using the project be sure you have `credentials.json` from **Google Cloud Console** and make sure **Google Sheets API** is enabled for your project.
+
+Then run `npm i` to install requirements and after you sure everything is good run the server with `node server.js`
+
+The server will be running on port 3000 by default. You can change the port by modifying the `port` variable in `server.js`.
---
+++ server.js
@@ -0,0 +1,133 @@
+import { google } from "googleapis";
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import fs from "fs";
+
+const app = express()
+app.use(cookieParser());
+app.use(express.json());
+
+// Configuration - sonradan .env fayılına atmaq lazımdı
+const port = 3000
+const spreadsheetId = "";
+const range = "Sheet1";
+
+const credentials = JSON.parse(fs.readFileSync("credentials.json", "utf8"));
+const users_data = JSON.parse(fs.readFileSync("users.json", "utf8"));
+const sessions = {};
+
+const client = new google.auth.GoogleAuth({
+    credentials,
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+});
+
+const sheets = google.sheets({ version: "v4", auth: client });
+
+app.post('/appointments', async (req, res) => {
+    const session_id = req.cookies.session_id;
+    const recieved_data = req.body;
+
+    if (!session_id || !sessions[session_id]) {
+        return res.status(401).json({ success: false, message: "Giriş etmədən bu səhifəyə daxil ola bilməzsiniz." });
+    }
+
+    const sheet_response = await sheets.spreadsheets.values.get({ spreadsheetId, range });
+
+    const rows = sheet_response.data.values;
+    const headers = rows[0];
+    const json_data = [];
+    
+    for (let i = 1; i < rows.length; i++) {
+        const rowData = {};
+        for (let j = 1; j < headers.length; j++) {
+            rowData[headers[j]] = rows[i][j] || "";
+        }
+
+        let filter_check = true;
+
+        if (recieved_data.ad && recieved_data.ad.trim() !== '') {
+            if (!rowData.NAME.split(" ")[0].toLowerCase().includes(recieved_data.ad.toLowerCase())) {
+                filter_check = false;
+            }
+        }
+
+        if (recieved_data.soyad && recieved_data.soyad.trim() !== '' && filter_check) {
+            if (!rowData.NAME.split(" ")[1].toLowerCase().includes(recieved_data.soyad.toLowerCase())) {
+                filter_check = false;
+            }
+        }
+
+        if (recieved_data.status && recieved_data.status.trim() !== '' && filter_check) {
+            if (!rowData.STATUS.toLowerCase().includes(recieved_data.status.toLowerCase())) {
+                filter_check = false;
+            }
+        }
+
+        if (recieved_data.date && recieved_data.date.trim() !== '' && filter_check) {
+            const dateOnly = rowData.DATE ? rowData.DATE.split(' ')[0] : "";
+            if (dateOnly !== recieved_data.date) {
+                filter_check = false;
+            }
+        }
+
+        if (recieved_data.fin && recieved_data.fin.trim() !== '' && filter_check) {
+            if (!rowData.FIN.toLowerCase().includes(recieved_data.fin.toLowerCase())) {
+                filter_check = false;
+            }
+        }
+
+        if (filter_check) {
+            json_data.push(rowData);
+        }
+    }
+
+    res.json(json_data);
+});
+
+app.get('/login', (req, res) => {
+    const username = req.query.username;
+    const password = req.query.password;
+    const cookie_session = req.cookies.session_id;
+    
+    if (cookie_session && sessions[cookie_session]) {
+        return res.json({ success: false, message: "Hal hazırda giriş etmiş vəziyyətdəsiniz." });
+    }
+
+    const user = users_data.find(user => user.username === username && user.password === password);
+    
+    if (user) {
+        var result = '';
+        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for (var i = 0; i < 48; i++) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+
+        sessions[result] = { username: username };
+
+        res.cookie("session_id", result, { maxAge: 43200000 });
+        res.json({ success: true, message: "Xoş gəlmişsiniz!" });
+
+    } else {
+        res.status(401);
+        res.json({ success: false, message: "Şifə səhvdir yenidən yoxlayın :(" });
+    }
+});
+
+app.get('/logout', (req, res) => {
+    const session_id = req.cookies.session_id;
+    if (session_id && sessions[session_id]) {
+        delete sessions[session_id];
+
+        res.clearCookie("session_id");
+        res.json({ success: true, message: "Hesabınızdan çıxış etdiniz." });
+    } else {
+        res.status(400);
+        res.json({ success: false, message: "Cookie tapılmadı." });
+    }
+
+});
+
+app.listen(port, () => {
+  console.log(`${port} portu bizimdir!`);
+})
